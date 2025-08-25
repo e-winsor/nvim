19c2404ad962a997cbbf114f7e2b4d29b33a4f1e
Edward Winsor <edward.winsor@pm.me>
2025-08-25 16:24:59 +0100

    moved .clang files back to home

--- lua/servers/clangd.lua
+++ lua/servers/clangd.lua
@@ -1,13 +1,10 @@
 return function(lspconfig, on_attach)
 	lspconfig.clangd.setup({
-        on_attach = on_attach,
-        cmd = {
-            "clangd",
-            "--offset-encoding=utf-16",
-            "--clang-tidy",
-            "--clang-tidy-config=" .. vim.fn.expand("~/.config/nvim/.clang-tidy"),
-            "--fallback-style=none",
-        },
+		on_attach = on_attach,
+		cmd = {
+			"clangd",
+			"--offset-encoding=utf-16",
+		},
 		filetypes = { "c", "cpp" },
 	})
 end
