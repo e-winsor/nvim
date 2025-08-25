1acbfabfff6bde944b634292aa21c2a8c0f4b9f8
Edward Winsor <edward.winsor@pm.me>
2025-08-25 15:19:39 +0100

    Added nvim-tree

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -38,7 +38,9 @@ vim.keymap.set("n", "J", "mzJ'z", {desc = "Join lines and keep cursor position"}
 -- Quick config editing
 vim.keymap.set("n", "<leader>rc", "<Cmd>e ~/.config/nvim/init.lua<CR>", {desc = "Edit condig"})
 
-
+-- File Explorer
+vim.keymap.set("n", "<leader>m", "<Cmd>NvimTreeFocus<CR>", { desc = "Focus on File Explorer" })
+vim.keymap.set("n", "<leader>e", "<Cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
 
 
 
